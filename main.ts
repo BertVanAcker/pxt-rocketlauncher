8f90890a3e45a19dc40202d43dbc75f8ccc84566
BertVanAcker <bert.van.acker@hotmail.nl>
2021-12-03 15:13:25 +0100

    v0.1

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
-})
+namespace rocketlauncher{
+    /**
+       * State of the rocket launcher
+       */
+    export enum RocketLaunchState {
+        //% block="Disarmed"
+        Disarmed,
+        //% block="Armed"
+        Armed,
+        //% block="Error"
+        Error
+    }
+
+    /**
+         * Select a traffic light state
+         * @param rocketState
+         */
+    //% blockId="set_rocket_state" block="Setting the rocket launcher state to %RocketLaunchState|"
+    //% weight=80 blockGap=8
+    export function RocketState(rocketState: RocketLaunchState): void {
+
+        if (rocketState == RocketLaunchState.Armed) {
+            //write pin 0 to HIGH 
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        }
+        else if (rocketState == RocketLaunchState.Disarmed){
+            //write pin 0 to LOW
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        }
+
+    }
+}
